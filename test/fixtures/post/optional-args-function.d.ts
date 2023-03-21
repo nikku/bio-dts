5a2c21915ef349b4bb9ff85e73dc6f8ba940e684
Nico Rehwaldt <git_nikku@nixis.de>
2023-03-21 16:22:32 +0100

    feat: support optional before required args for default function exports

--- test/fixtures/post/optional-args-function.d.ts
+++ test/fixtures/post/optional-args-function.d.ts
@@ -1,71 +1,33 @@
 /**
  * YO!
  *
- * @param event
- * @param callback
- * @param context
+ * @param {string} event
+ * @param {number} [priority]
+ * @param {Function} callback
+ * @param {any} [context]
  */
-declare function on(event: string, callback: Function, context?: any): void
+declare function on(event: string, priority?: number, callback: Function, context?: any): void;
 
 /**
  * YO!
  *
- * @param event
- * @param priority
- * @param callback
- * @param context
+ * @param {string} event
+ * @param {number} [priority]
+ * @param {Function} callback
+ * @param {any} [context]
  */
-declare function on(event: string, priority: number, callback: Function, context?: any): void
-
-declare function woop(boop: string): string
-declare function woop(event: string, boop: string): string
-declare function woop(boop: string, ...yea: any[]): string
-declare function woop(event: string, boop: string, ...yea: any[]): string
+export function onExported(event: string, priority?: number, callback: Function, context?: any): void;
 
 /**
  * YO!
  *
- * @param event
- * @param callback
- * @param context
+ * @param {string} event
+ * @param {number} [priority]
+ * @param {Function} callback
+ * @param {any} [context]
  */
-export declare function on(event: string, callback: Function, context?: any): void
+export default function onDefaultExported(event: string, priority?: number, callback: Function, context?: any): void;
 
-/**
- * YO!
- *
- * @param event
- * @param priority
- * @param callback
- * @param context
- */
-export declare function on(event: string, priority: number, callback: Function, context?: any): void
+declare function woop(event?: string, boop: string, ...yea: any[]): string;
 
-export declare function woop(boop: string): string
-export declare function woop(event: string, boop: string): string
-export declare function woop(boop: string, ...yea: any[]): string
-export declare function woop(event: string, boop: string, ...yea: any[]): string
-
-/**
- * YO!
- *
- * @param event
- * @param callback
- * @param context
- */
-export default function on(event: string, callback: Function, context?: any): void
-
-/**
- * YO!
- *
- * @param event
- * @param priority
- * @param callback
- * @param context
- */
-export default function on(event: string, priority: number, callback: Function, context?: any): void
-
-export default function woop(boop: string): string
-export default function woop(event: string, boop: string): string
-export default function woop(boop: string, ...yea: any[]): string
-export default function woop(event: string, boop: string, ...yea: any[]): string
+export function waap(event?: string, boop: string, ...yea: any[]): string;
--- test/fixtures/post/optional-args-function.expected.d.ts
+++ test/fixtures/post/optional-args-function.expected.d.ts
@@ -17,55 +17,45 @@ declare function on(event: string, callback: Function, context?: any): void
  */
 declare function on(event: string, priority: number, callback: Function, context?: any): void
 
-declare function woop(boop: string): string
-declare function woop(event: string, boop: string): string
+/**
+ * YO!
+ *
+ * @param event
+ * @param callback
+ * @param context
+ */
+export function onExported(event: string, callback: Function, context?: any): void;
+
+/**
+ * YO!
+ *
+ * @param event
+ * @param priority
+ * @param callback
+ * @param context
+ */
+export function onExported(event: string, priority: number, callback: Function, context?: any): void;
+
+/**
+ * YO!
+ *
+ * @param event
+ * @param callback
+ * @param context
+ */
+export default function onDefaultExported(event: string, callback: Function, context?: any): void;
+
+/**
+ * YO!
+ *
+ * @param event
+ * @param priority
+ * @param callback
+ * @param context
+ */
+export default function onDefaultExported(event: string, priority: number, callback: Function, context?: any): void;
+
 declare function woop(boop: string, ...yea: any[]): string
 declare function woop(event: string, boop: string, ...yea: any[]): string
-
-/**
- * YO!
- *
- * @param event
- * @param callback
- * @param context
- */
-export declare function on(event: string, callback: Function, context?: any): void
-
-/**
- * YO!
- *
- * @param event
- * @param priority
- * @param callback
- * @param context
- */
-export declare function on(event: string, priority: number, callback: Function, context?: any): void
-
-export declare function woop(boop: string): string
-export declare function woop(event: string, boop: string): string
-export declare function woop(boop: string, ...yea: any[]): string
-export declare function woop(event: string, boop: string, ...yea: any[]): string
-
-/**
- * YO!
- *
- * @param event
- * @param callback
- * @param context
- */
-export default function on(event: string, callback: Function, context?: any): void
-
-/**
- * YO!
- *
- * @param event
- * @param priority
- * @param callback
- * @param context
- */
-export default function on(event: string, priority: number, callback: Function, context?: any): void
-
-export default function woop(boop: string): string
-export default function woop(event: string, boop: string): string
-export default function woop(boop: string, ...yea: any[]): string
-export default function woop(event: string, boop: string, ...yea: any[]): string
+export function waap(boop: string, ...yea: any[]): string;
+export function waap(event: string, boop: string, ...yea: any[]): string;
