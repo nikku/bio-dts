394b922592824fef31a08108488adf7e853178ef
Nico Rehwaldt <git_nikku@nixis.de>
2025-01-10 22:37:00 +0100

    fix!: generate overloads in definition order
    This aligns with how TypeScript generates these definitions (with @overload).
    
    Cf. https://github.com/nikku/bio-dts/issues/5#issuecomment-2580329122

--- test/fixtures/post/overload-optional-args.d.ts
+++ test/fixtures/post/overload-optional-args.d.ts
@@ -1,15 +1,15 @@
 /**
  * @overlord
  *
- * Some other method description, o yea!
- *
- * @param {string} [event]
- * @param {any} obj
- */
-/**
  * Some method description, o yea!
  *
  * @param {string} [event]
  * @param {number} obj
+ */
+/**
+ * Some other method description, o yea!
+ *
+ * @param {string} [event]
+ * @param {any} obj
  */
 export function exportOptionalArg(event?: string, obj: any) : void;
