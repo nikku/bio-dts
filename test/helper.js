bec8ac2daa71f4fb9ba6bdb42305650faadce068
Nico Rehwaldt <git_nikku@nixis.de>
2025-12-29 23:19:45 +0100

    test: simplify file reading

--- test/helper.js
+++ test/helper.js
@@ -1,10 +1,5 @@
-import URL from 'node:url';
 import fs from 'node:fs';
-import path from 'node:path';
-
 
 export function readFile(file) {
-  const dirname = path.dirname(URL.fileURLToPath(import.meta.url));
-
-  return fs.readFileSync(path.join(dirname, file), 'utf8');
+  return fs.readFileSync(new URL(file, import.meta.url), 'utf8');
 }
