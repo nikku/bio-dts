61944ad52ed30548fdf48ab190c6cc4fc173f70d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-06-05 13:14:04 +0200

    test: validate transforms don't touch import declarations

---
+++ test/fixtures/pre/jsdoc-esm.js
@@ -0,0 +1,12 @@
+/**
+ * Great file!
+ */
+
+/**
+ * Foo
+ *
+ * @param {import('./Bar.js').default} bar
+ */
+export function Foo(bar) {
+
+}
---
+++ test/fixtures/snapshots/declaration-map/jsdoc-esm.expected.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Great file!
+ */
+/**
+ * Foo
+ *
+ */
+export class Foo {
+    /**
+     * @param bar
+     */
+    constructor(bar: import("./Bar.js").default);
+}
+//# sourceMappingURL=jsdoc-esm.expected.d.ts.map
